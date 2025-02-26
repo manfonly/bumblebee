921289e8dbf45c90ee574bdc2392851cc2bbb32b
manfonly <manfonly@gmail.com>
2025-02-26 20:16:17 +0800

    fill the screen

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+import GameScene from './game/scenes/GameScene';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        parent: 'game',
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: GameScene
+};
+
+new Phaser.Game(config);
